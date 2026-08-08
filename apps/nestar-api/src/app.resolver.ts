1d162f540180f5914db9c0ffbf83a1e8baa5276c
Anthony <asadbeksaydullayev2911@gmail.com>
2026-08-08 13:45:03 +0900

    feat: integration GRPHQLModule & build first GrphQl API

---
+++ apps/nestar-api/src/app.resolver.ts
@@ -0,0 +1,11 @@
+import { Query, Resolver } from "@nestjs/graphql";
+import { query } from "express";
+
+
+@Resolver()
+export class AppResolver {
+    @Query(() => String)
+    public sayHello(): string {
+        return "GraphQL API Server"
+    }
+}
