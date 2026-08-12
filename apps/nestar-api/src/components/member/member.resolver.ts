2c40efc8c72533a82c68bea0d73c0048c50ae830
Anthony <asadbeksaydullayev2911@gmail.com>
2026-08-12 20:51:45 +0900

    Feat: develop signUp business Logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,30 +1,50 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { InternalServerErrorException, UsePipes, ValidationPipe } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { Member } from '../../libs/dto/member/member';
 
 @Resolver()
 export class MemberResolver {
     constructor(private readonly memberService: MemberService) { };
 
-    @Mutation(() => String)
+    @Mutation(() => Member)
     @UsePipes(ValidationPipe)
-    public async signUp(@Args("input") input: MemberInput): Promise<string> {
-        console.log("Mutation signUp");
-        console.log("input: ", input);
-        return this.memberService.signUp();
+    public async signUp(@Args("input") input: MemberInput): Promise<Member> {
+        try {
+            console.log("Mutation signUp");
+            console.log("input: ", input);
+            return this.memberService.signUp(input);
+        }
+        catch (err) {
+            console.log("ERROR, signUp: ", err);
+            throw new InternalServerErrorException(err);
+        }
     }
 
     @Mutation(() => String)
-    public async login(@Args("input") input: LoginInput): Promise<string> {
-        console.log("Mutation login");
-        return this.memberService.login();
+    @UsePipes(ValidationPipe)
+    public async login(@Args("input") input: LoginInput): Promise<Member> {
+        try {
+            console.log("Mutation login");
+            return this.memberService.login(input);
+        }
+        catch (err) {
+            console.log("ERROR, login: ", err);
+            throw new InternalServerErrorException(err);
+        }
     }
 
     @Mutation(() => String)
     public async updateMember(): Promise<string> {
-        console.log("Mutation updateMember");
-        return this.memberService.UpdateMember();
+        try {
+            console.log("Mutation updateMember");
+            return this.memberService.UpdateMember();
+        }
+        catch (err) {
+            console.log("ERROR, UpdateMember: ", err);
+            throw new InternalServerErrorException(err);
+        }
     }
 
 
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,18 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Member } from '../../libs/dto/member/member';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 
 @Injectable()
 export class MemberService {
 
-    constructor(@InjectModel("Member") private readonly memberModel: Model<null>) { }
+    constructor(@InjectModel("Member") private readonly memberModel: Model<Member>) { }
 
-    public async signUp(): Promise<string> {
-        return "SignUp Executed!"
+    public async signUp(input: MemberInput): Promise<Member> {
+        //TODO: HASH Password
+        try {
+            //TODO: Auth via tokens
+            const result = await this.memberModel.create(input);
+            return result
+        }
+        catch (err) {
+            console.log("ERROR, signUp", err)
+            throw new BadRequestException(err)
+        }
+
     }
 
-    public async login(): Promise<string> {
-        return "Login Executed!"
+    public async login(input: LoginInput): Promise<Member> {
+        try {
+            const result = await this.memberModel.findOne(
+
+            )
+            return result;
+        }
+        catch (err) {
+            console.log("ERROR, login", err)
+            throw new BadRequestException(err)
+        }
     }
 
     public async UpdateMember(): Promise<string> {
