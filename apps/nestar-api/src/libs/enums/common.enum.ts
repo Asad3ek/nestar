dee18e6e3b293962316c12ff1532b485f11721d4
Anthony <asadbeksaydullayev2911@gmail.com>
2026-08-26 20:24:31 +0900

    fix: modified upload images configuration via graphqlUploadExpress

--- apps/nestar-api/src/libs/enums/common.enum.ts
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -19,7 +19,9 @@ export enum Message {
     ONLY_SPECIFIC_ROLES_ALLOWED = "Allowed only for members with specific roles",
     NOT_ALLOWED_REQUEST = "Not allowed request",
     USED_NICK_OR_PHONE = "Already Used Member Nick or Phone!",
-    PROVIDE_ALLOWED_FORMAT = "PROVIDE_ALLOWED_FORMAT",
+    PROVIDE_ALLOWED_FORMAT = "Please provide jpg, jpeg or png images!",
+    SELF_SUBSCRIPTION_DENIED = "Self subscription is denied!",
+
 }
 
 
