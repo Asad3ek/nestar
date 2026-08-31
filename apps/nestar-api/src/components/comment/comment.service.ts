ea6373d3f3d959cac1e9c4d6152dca10847a981f
Anthony <asadbeksaydullayev2911@gmail.com>
2026-09-01 02:36:51 +0900

    feat: develop removeCommentByAdmin GRAPHQL API

--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -105,4 +105,10 @@ export class CommentService {
     }
 
 
+    public async removeCommentByAdmin(input: ObjectId): Promise<Comment> {
+        const result = await this.commentModel.findByIdAndDelete(input);
+        if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+        return result;
+    }
+
 }
