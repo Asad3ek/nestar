42a63d38477ed982e5880add7e31662e9f627f78
Anthony <asadbeksaydullayev2911@gmail.com>
2026-09-03 00:45:34 +0900

    created resolver & service modules based on Following

--- apps/nestar-api/src/components/follow/follow.module.ts
+++ apps/nestar-api/src/components/follow/follow.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import FollowSchema from '../../schemas/Follow.model';
+import { AuthModule } from '../auth/auth.module';
+import { FollowService } from './follow.service';
+import { FollowResolver } from './follow.resolver';
+import { MemberModule } from '../member/member.module';
 
-@Module({})
-export class FollowModule {}
+@Module({
+    imports: [MongooseModule.forFeature([
+        { name: "Follow", schema: FollowSchema }
+    ]),
+        AuthModule,
+        MemberModule
+    ],
+    providers: [FollowResolver, FollowService],
+    exports: [FollowService]
+})
+export class FollowModule { }
