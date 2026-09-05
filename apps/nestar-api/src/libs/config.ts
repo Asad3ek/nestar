a493f72c170777d5227d7caf88e6f59be59eb0f2
Anthony <asadbeksaydullayev2911@gmail.com>
2026-09-05 15:55:01 +0900

    Feat: integration of lookupAuthMemberLiked complex query business logic

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,5 +1,11 @@
 import { ObjectId } from "bson";
 
+/** IMAGE CONFIGURATION **/
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
+import { T } from "./types/common";
+import { pipeline } from "stream";
+
 export const availableAgentSorts = ['createdAt', "updatedAt", 'memberLikes', 'memberViews', 'memberRank']
 export const availableMemberSorts = ['createdAt', "updatedAt", 'memberLikes', 'memberViews']
 
@@ -17,11 +23,39 @@ export const availablePropertySorts = [
 export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
 export const availableCommentSorts = ['createdAt', 'updatedAt'];
 
+export const lookupAuthMemberLiked = (memberId: T, targetRefId: string = "$_id") => {
+    return {
+        $lookup: {
+            from: "likes",
+            let: {
+                localLikeRefId: targetRefId,
+                localMemberId: memberId,
+                localMyFavorite: true,
+            },
+            pipeline: [
+                {
+                    $match: {
+                        $expr: {
+                            $and: [{ $eq: ["$likeRefId", "$$localLikeRefId"] }, { $eq: ["$memberId", "$$localMemberId"] }],
+                        },
+                    },
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        memberId: 1,
+                        likeRefId: 1,
+                        myFavorite: "$$localMyFavorite"
+                    }
+                },
+            ],
+            as: "meLiked",
+        },
+    };
+
+};
 
 
-/** IMAGE CONFIGURATION **/
-import { v4 as uuidv4 } from 'uuid';
-import * as path from 'path';
 
 export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
 export const getSerialForImage = (filename: string) => {
